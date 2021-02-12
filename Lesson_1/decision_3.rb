c34a0cfaab4e1e30a1ef97d164cfcf43d3812511
gringbeeri <vladgashko@gmail.com>
2021-02-13 00:53:51 +0300

    correction

--- Lesson_1/decision_3.rb
+++ Lesson_1/decision_3.rb
@@ -1,12 +1,24 @@
 puts "Введите сторону 1:"
-side_1 = gets.to_f 
+side_1 = gets.to_i
 puts "Введите сторону 2:"
-side_2 = gets.to_f
+side_2 = gets.to_i
 puts "Введите сторону 3:"
-side_3 = gets.to_f
-# list_side = [side_1, side_2, side_3]
-# max_side = list_side.sort.last
-# another = [side_1, side_2, side_3]
+side_3 = gets.to_i
+list_side = [side_1, side_2, side_3]
+max_side = [side_1, side_2, side_3].max
+list_side.delete(max_side)
+a, b = list_side
+if (a.to_i + b.to_i) < max_side
+    puts "Треугольника не сущетсвует"
+end
+
+if side_1 && side_2 && side_3
+    puts "Треугольник равносторонний"
+elsif a && b || a && c || b && c
+    puts "Треугольник равнобедренный"
+elsif max_side ** 2 && a ** 2 + b ** 2
+    puts "Треугольник прямоугольный"
+end
 
     
 
